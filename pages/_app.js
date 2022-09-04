6f5714b8501c8f617f257eb2a363657c9b19232d
NaimIslamAntor <naimfn24@gmail.com>
2022-09-04 11:45:51 +0600

    adding router progress bar

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,38 @@ import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
 
+import { useRouter } from 'next/router'
+import NProgress from 'nprogress'
+import { useEffect } from 'react'
+
+
+
 function MyApp({ Component, pageProps }) {
+
+
+  const router = useRouter()
+
+  
+  useEffect(() => {
+    const handleStart = (url) => {
+      NProgress.start()
+    }
+    const handleStop = () => {
+      NProgress.done()
+    }
+
+    router.events.on('routeChangeStart', handleStart)
+    router.events.on('routeChangeComplete', handleStop)
+    router.events.on('routeChangeError', handleStop)
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart)
+      router.events.off('routeChangeComplete', handleStop)
+      router.events.off('routeChangeError', handleStop)
+    }
+  }, [router])
+
+
   return(
     <>
     <ContextProvider>
